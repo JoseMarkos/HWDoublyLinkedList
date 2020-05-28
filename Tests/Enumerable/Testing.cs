dcc6fb28c4ba44c431656879df4a6ac5eb6aabc3
Marcos <jkmarcosb@gmail.com>
2020-05-27 23:25:24 -0600

    changing enumerable implementation in tests and rifht move next order

--- Tests/Enumerable/Testing.cs
+++ Tests/Enumerable/Testing.cs
@@ -7,45 +7,41 @@ using System.Collections.Generic;
 
 namespace HWDoublyLinkedList.Tests.Enumerable {
     public class Testing {
-
         private List AddFirstAddAfterFirstAddAfterFirst() {
             List list = new List();
             Node newNode = new Node(1);
             list.InsertFirst(newNode);
-
             Node newNode2 = new Node(3);
             list.InsertAfterFirst(newNode2);
-
             Node newNode3 = new Node(2);
             list.InsertAfterFirst(newNode3);
 
             return list;
         }
 
-        [Fact] 
-        public void EnumerableListParamenterEquals() {
-            List list = AddFirstAddAfterFirstAddAfterFirst();
-
-            ListEnumerable enumerable = new ListEnumerable(list);
-
-            Assert.Equal(list, enumerable.List);
-        }
-
         [Fact]
         public void EnumeratorMoveNext() {
             List list = AddFirstAddAfterFirstAddAfterFirst();
-
-            ListEnumerable enumerable = new ListEnumerable(list);
-            IEnumerator<Node> enumerator = enumerable.GetEnumerator();
+            IEnumerator<Node> enumerator = list.GetEnumerator();
 
             while(enumerator.MoveNext())
             {
-                
             }
             
             Assert.Equal(3, enumerator.Current.Data);
         }
 
+        [Fact]
+        public void MoveNext() {
+            List list = AddFirstAddAfterFirstAddAfterFirst();
+            IEnumerator<Node> enumerator = list.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.Equal(1, enumerator.Current.Data);
+            enumerator.MoveNext();
+            Assert.Equal(2, enumerator.Current.Data);
+            enumerator.MoveNext();
+            Assert.Equal(3, enumerator.Current.Data);
+        }
         // Dispose
     }
 }
